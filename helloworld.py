b2c4ffa66e791fa47a8010eb93dbd5b7018b3670
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-02-11 19:09:44 +0100

    Quelques commentaires, no more

--- helloworld.py
+++ helloworld.py
@@ -6,14 +6,14 @@ print(100)
 # Hello world
 
 print("Salut, Terrien !")
-couleur_favorite = "bleu"
-print(couleur_favorite)
-print(couleur_favorite*10, sep=" ")
+couleur_favorite = "bleu "
+print(couleur_favorite + ": 1 fois")
+print(couleur_favorite*10 + ": 10 fois", sep=" ")
 
 # Functions
 
 print(pow(4, 2))            # Carré de 4
-print(int(3.14))
+print(int(3.14))            # Partie entière de pi
 print("2*2", 2*int("2"))    # Parse a string to the int function
 print(type(2.5))            # Print data type
 
@@ -23,8 +23,8 @@ def f(x,w) : return w*x
 w=4
 x=2
 yp=f(x,w)
-print(yp)
+print(yp, "... pas de classificateur sans fonction ...")
 
 # Input
-n = input("Please enter your name: ")
-print("Hello", n, "!")
+n = input("Ton nom ? Comme en Basic avec input : ")
+print("Salut", n, "!")
