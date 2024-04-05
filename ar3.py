111933cd3ca16e7153e7fc19f664b307ae098cdf
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-04-05 15:41:29 +0200

    Forme

--- ar3.py
+++ ar3.py
@@ -11,7 +11,6 @@ def generate_AR3(phi, n):
     # Generate AR(3) process
     for t in range(3, n):
         X[t] = phi[0]*X[t-1] + phi[1]*X[t-2] + phi[2]*X[t-3] + epsilon[t]
-    
     return X
 
 # Generate random AR parameters
@@ -20,6 +19,7 @@ phi = np.random.uniform(-1, 1, size=3)
 # Check stationarity
 characteristic_poly = np.poly1d([1, -phi[0], -phi[1], -phi[2]])
 roots = np.roots(characteristic_poly)
+
 if np.all(np.abs(roots) > 1):
     print("AR(3) process is stationary.")
 else:
