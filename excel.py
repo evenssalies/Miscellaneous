f3353e72d0aa2573be27f6885ff9f2e98a4d1907
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-05-06 01:35:15 +0200

    Charge un fichier excel

---
+++ excel.py
@@ -0,0 +1,8 @@
+# Avant d'exécuter, installer openpyxl :
+#   pip install openpyxl
+
+import pandas as pd
+
+df = pd.read_excel("C:/Users/evens/Documents/Evens/DATA/RDI/Scoreboard_panel.xlsx")
+
+print(df)
