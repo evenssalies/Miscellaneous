2186a531cacbca6bad0115a5430d1106bc69d6a9
Evens Salies <32770815+evenssalies@users.noreply.github.com>
2024-10-17 10:19:38 +0200

    Make the code neutral for most usage with 'myfile', 'mydata' ...

--- importexport.py
+++ importexport.py
@@ -2,36 +2,24 @@
 #
 #   .dta
 import pandas as pd
-DATA = pd.read_stata('http://www.evens-salies.com/rd_e_gerdfund.dta')
+DATA = pd.read_stata('myfile.dta')
 
-#       Les variables du fichier, missing et type
-DATA.info()
-
-#       Première valeur de la variable RD_COU_TOTAL_TOTAL
-VAR = DATA.at[0, 'RD_COU_TOTAL_TOTAL']
-print(VAR)
-
-#   .raw
+#   .raw (do later)
 
 #   .csv
-DATA = pd.read_csv("http://www.evens-salies.com/country_code_baci92.csv")
-DATA.info()
-for j in range(len(DATA)):
-    print(DATA.at[j, 'country'], DATA.at[j, 'i'])
+DATA = pd.read_csv('myfile.csv')
 
 #   .xlsx
-DATA = pd.read_excel('C:/Users/evens/Documents/cardkrueger1994_short.xlsx',
-        sheet_name='Feuil2',
+DATA = pd.read_excel('myfile.xlsx',
+        sheet_name='sheetname',
         header=0)
-DATA.info()
 
 #   .RData
 import pyreadr as r
-FRAME = r.read_r('C:/Users/evens/Documents/Synth/data/synth.data.RData')
-DATA = FRAME['synth.data']
-DATA.info()
+FRAME = r.read_r('myfile.RData')
+DATA = FRAME['mydata.data']
 
 # Exporter
 #
 #   .dta
-DATA.to_stata('C:/Users/evens/Documents/synth.data.dta', write_index=False)
+DATA.to_stata('filename.dta', write_index=False)
