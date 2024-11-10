ba32cbfda2e93852225a5d2e6bbdde1682ac920d
evenssalies <32770815+evenssalies@users.noreply.github.com>
2024-11-10 15:35:33 +0100

    L'import de classeurs est désormais dans importexport.py

--- excel.py
+++
@@ -1,8 +0,0 @@
-# Avant d'exécuter, installer openpyxl :
-#   pip install openpyxl
-
-import pandas as pd
-
-df = pd.read_excel("C:/Users/evens/Documents/Evens/DATA/RDI/Scoreboard_panel.xlsx")
-
-print(df)
