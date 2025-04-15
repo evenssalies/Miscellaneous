3b7d00a00d967e467eef2e93a0e35d33b0c324b6
Evens Salies <32770815+evenssalies@users.noreply.github.com>
2025-04-15 17:27:06 +0200

    Update nobscsv.py

--- nobscsv.py
+++ nobscsv.py
@@ -1,22 +1,18 @@
-# Cette routine obtient le nombre de lignes d'un gros fichier .csv sans le charger complètement.
-#   Nous l'appliquons au fichier de 8,3 Go de recensement des établissements localisés en France
-#       StockEtablissementHistorique_utf8.csv, disponible sur data.gouv.fr :
-#       Pour le télécharger directement :
-#       https://www.data.gouv.fr/en/datasets/base-sirene-des-entreprises-et-de-leurs-etablissements-siren-siret/
+# This routine gets the number of lines in a large .csv file without loading it completely.
 #
-# Auteur: Evens SALIES
+#    Evens SALIES, v1. 12/2024
 
 import pandas as pd
 
-# Pareil, mais avec la fonction read_csv de pandas et en utilisant des chunks
+# The chunksize argument specifies the number of rows to read at a time when processing the file in chunks.
 def count_lines_in_csv(file_path):
     line_count = 0
     chunk_size = 1000000
-    for chunk in pd.read_csv(file_path, chunksize=chunk_size, dtype=str):
+    for chunk in pd.read_csv(file_path, chunksize = chunk_size, dtype = str):
         line_count += len(chunk)
         print(f"We reached", line_count)
     return line_count
 
 # Replace the path with the path to your file
-line_count = count_lines_in_csv('C:/Users/evens/Documents/Evens/DATA/Firm/Demography/StockEtablissementHistorique_utf8.csv')
+line_count = count_lines_in_csv('pathtocsvfilehere.csv')
 print(f"The CSV file contains {line_count} lines.")
